926e68fc4fcc19548105f4f5a23840529eb0020b
skyboy58568 <chisreerag@gmail.com>
2024-11-11 19:18:50 +0530

    Restore local history

--- src/test/kotlin/org/agileSoftDev/helpers/FIxtures.kt
+++ src/test/kotlin/org/agileSoftDev/helpers/FIxtures.kt
@@ -1,6 +1,7 @@
 package org.agileSoftDev.helpers
 
 import org.agileSoftDev.domain.Activity
+import org.agileSoftDev.domain.HealthIndicator
 import org.agileSoftDev.domain.User
 import org.joda.time.DateTime
 
@@ -11,8 +12,9 @@ val validEmail = "testuser1@test.com"
 val users = arrayListOf<User>(
     User(name = "Alice Wonderland", email = "alice@wonderland.com", id = 1, role = "user", password = "abc"),
     User(name = "Bob Cat", email = "bob@cat.ie", id = 2, role = "user", password = "abc"),
-    User(name = "Mary Contrary", email = "mary@contrary.com", id = 3, role ="abc" ),
-    User(name = "Carol Singer", email = "carol@singer.com", id = 4, role ="abcr" )
+    User(name = "Mary Contrary", email = "mary@contrary.com", id = 3, role ="user", password = "abc"),
+    User(name = "Carol Singer", email = "carol@singer.com", id = 4, role ="user" , password = "abc"),
+    User(name= "Sreerag Sathian", email = "20109451@mail.wit.ie", id = 5, role = "admin", password = "abc"),
 )
 
 val activities = arrayListOf<Activity>(
@@ -23,7 +25,18 @@ val activities = arrayListOf<Activity>(
 
     Activity(id = 12, description = "Jump roping", duration = 15.1, calories = 9, started = DateTime.now(), userId = 3),
 
-     Activity(id = 13, description = "running", duration = 20.1, calories = 13, started = DateTime.now(), userId = 4)
+    Activity(id = 13, description = "running", duration = 20.1, calories = 13, started = DateTime.now(), userId = 4)
+
+)
+
+
+val healthindicators = arrayListOf<HealthIndicator>(
+
+    HealthIndicator(indicatorid = 1, userid = 4, age=30, height = 177, weight = 68, boxygen = 98, hdl = 60, ldl = 121, alt = 40, ast = 50, gfr = 80) ,
+    HealthIndicator(indicatorid = 2, userid = 3, age=30, height = 177, weight = 68, boxygen = 98, hdl = 60, ldl = 121, alt = 40, ast = 50, gfr = 80),
+    HealthIndicator(indicatorid = 2, userid = 3, age=30, height = 177, weight = 68, boxygen = 98, hdl = 62, ldl = 97, alt = 33, ast = 33, gfr = 80)
+
+
 
 )
 
--- src/test/kotlin/org/agileSoftDev/repositiries/healthIndicatorDAOTest.kt
+++ src/test/kotlin/org/agileSoftDev/repositiries/healthIndicatorDAOTest.kt
@@ -1,4 +1,46 @@
 package org.agileSoftDev.repositiries
 
+import org.agileSoftDev.domain.HealthIndicator
+import org.agileSoftDev.domain.db.HealthIndicators
+import org.agileSoftDev.domain.db.Users
+import org.agileSoftDev.domain.repository.HealthIndicatorDAO
+import org.agileSoftDev.domain.repository.UserDAO
+import org.agileSoftDev.helpers.healthindicators
+import org.agileSoftDev.helpers.users
+import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.SchemaUtils
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.Nested
+
 class healthIndicatorDAOTest {
+
+    private val healthIndicatorDAO = HealthIndicatorDAO()
+    private val userDAO = UserDAO()
+
+    companion object {
+        @BeforeAll
+        @JvmStatic
+        internal fun setupInMemoryDatabaseConnection() {
+            Database.connect("jdbc:h2:mem:test", driver = "org.h2.Driver", user = "root", password = "")
+        }
+    }
+
+    fun setUpScheamas() : HealthIndicatorDAO{
+
+
+        SchemaUtils.create(Users)
+        SchemaUtils.create(HealthIndicators)
+        for (user in users) {
+
+            userDAO.addUser(user)
+
+        }
+        healthIndicatorDAO.addHealthIndicators(healthindicators)
+        return  healthIndicatorDAO
+
+
+    }
+
+
+
 }
