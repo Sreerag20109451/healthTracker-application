1f94b83340b5e8639c73d4ae63bc9a3f430589a1
skyboy58568 <chisreerag@gmail.com>
2024-12-18 19:55:10 +0530

    Chane CORS rule to accept anyhost

--- src/main/kotlin/org/agileSoftDev/config/JavalinConfig.kt
+++ src/main/kotlin/org/agileSoftDev/config/JavalinConfig.kt
@@ -23,9 +23,7 @@ class JavalinConfig {
             it.jsonMapper(JavalinJackson(jsonObjectMapper()))
             it.bundledPlugins.enableCors { cors ->
                 cors.addRule { crs ->
-                    crs.allowHost("http://localhost:8080")
-                    crs.allowHost("https://frontend-healthtracker-production.up.railway.app/")
-                    crs.allowCredentials = true
+                    crs.anyHost()
                 }
             }
 
